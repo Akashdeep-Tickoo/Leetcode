7b81e166d041f529b738bd3750c2b779bb196003
Akashdeep Tickoo <akashdeeptickoo2514@gmail.com>
2022-09-28 00:00:00 +0530

    Time: 100 ms (24.69%), Space: 43.6 MB (21.78%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void comb(vector<int>& candidates,vector<vector<int>>& ans,vector<int>ds,int index,int target)
+    {
+        if(index==candidates.size())
+        {
+            if(target==0)
+            {
+                ans.push_back(ds);
+            }
+            return;
+        }
+        if(candidates[index]<=target)
+        {
+            ds.push_back(candidates[index]);
+            comb(candidates,ans,ds,index,target-candidates[index]);
+            ds.pop_back();
+        }
+        comb(candidates,ans,ds,index+1,target);
+        
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>>ans;
+        vector<int>ds;
+        comb(candidates,ans,ds,0,target);
+        return ans;
+    }
+};
