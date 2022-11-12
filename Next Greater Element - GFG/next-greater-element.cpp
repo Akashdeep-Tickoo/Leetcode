28efe23942cb9d96ff4da255561f535badf7d671
Akashdeep Tickoo <akashdeeptickoo2514@gmail.com>
2022-11-12 16:45:41 +0530

    Added solution - LeetHub

--- Next Greater Element - GFG/next-greater-element.cpp
+++ Next Greater Element - GFG/next-greater-element.cpp
@@ -4,9 +4,6 @@ using namespace std;
 
 
 // } Driver Code Ends
-
-#include<bits/stdc++.h>
-using namespace std;
 class Solution
 {
     public:
@@ -17,39 +14,38 @@ class Solution
         stack<long long> s;
         for(int i=n-1;i>=0;--i)
         {
-            if(s.size()==0)
+            if(s.empty())
             {
                 v.push_back(-1);
                 s.push(arr[i]);
             }
+            else if(arr[i]<s.top())
+            {
+                v.push_back(s.top());
+                s.push(arr[i]);
+            }
             else
             {
-                if(s.top()>arr[i])
+                while(s.size()!=0&&s.top()<=arr[i])
+                {
+                    s.pop();
+                }
+                if(s.empty())
+                {
+                    v.push_back(-1);
+                    s.push(arr[i]);
+                }
+                else
                 {
                     v.push_back(s.top());
                     s.push(arr[i]);
-                }
-                else{
-                    while(s.size()!=0&&s.top()<=arr[i])
-                    {
-                        s.pop();
-                    }
-                    if(s.size()>0&&s.top()>arr[i])
-                    {
-                        v.push_back(s.top());
-                        s.push(arr[i]);
-                    }
-                    else if(s.size()==0)
-                    {
-                        v.push_back(-1);
-                        s.push(arr[i]);
-                    }
                 }
             }
         }
         reverse(v.begin(),v.end());
         return v;
     }
+    
 };
 
 //{ Driver Code Starts.
