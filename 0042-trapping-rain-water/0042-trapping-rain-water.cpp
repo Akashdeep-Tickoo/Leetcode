8f2c5fb68e198e80ac1283e0061c4b1104b58ba6
Akashdeep Tickoo <akashdeeptickoo2514@gmail.com>
2022-11-20 02:04:19 +0530

    Time: 19 ms (87.38%), Space: 21.8 MB (5.26%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -2,42 +2,33 @@
 using namespace std;
 class Solution {
 public:
-    int trap(vector<int>& height) { 
-        int ans=0;
-        pair<int,int> p;
-        p={-1,-1};
-        vector<pair<int,int>> left;
-        vector<pair<int,int>> right;
+    int trap(vector<int>& height) {
+        int lmaxi=height[0];
+        int rmaxi=height[height.size()-1];
+        vector<int> left;
+        vector<int> right;
         for(int i=0;i<height.size();++i)
         {
-            left.push_back(p);
-            if(p.first<height[i])
+            if(height[i]>lmaxi)
             {
-                p={height[i],i};
+                lmaxi=height[i];
             }
+            left.push_back(lmaxi);
         }
-        p={-1,-1};
         for(int i=height.size()-1;i>=0;--i)
         {
-            right.push_back(p);
-            if(p.first<height[i])
+            if(height[i]>rmaxi)
             {
-                p={height[i],i};
+                rmaxi=height[i];
             }
+            right.push_back(rmaxi);
         }
         reverse(right.begin(),right.end());
+        int sum=0;
         for(int i=0;i<height.size();++i)
         {
-            if(left[i].second!=-1&&right[i].second!=-1)
-            {
-                int sum=min(right[i].first,left[i].first);
-                sum=sum-height[i];
-                if(sum>=0)
-                {
-                    ans+=sum;
-                }
-            }
+            sum+=min(left[i],right[i])-height[i];
         }
-        return ans;
+        return sum;
     }
 };
