8b5923ed8edef817a81b301382e5e195b1caff97
Akashdeep Tickoo <akashdeeptickoo2514@gmail.com>
2023-03-17 14:32:58 +0530

    Time: 25 ms (93.38%), Space: 26.6 MB (74.81%) - LeetHub

--- 0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.cpp
+++ 0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.cpp
@@ -3,31 +3,56 @@ using namespace std;
 class Solution {
 public:
     int findUnsortedSubarray(vector<int>& nums) {
-        vector<int>arr(nums);
-        sort(arr.begin(),arr.end());
-        int si=-1;
-        int ei=-1;
+        int snum=INT_MAX;
+        int lnum=INT_MIN;
         int n=nums.size();
-        for(int i=0;i<n;++i)
-        {
-            if(nums[i]!=arr[i])
-            {
-                si=i;
-                break;
-            }
-        }
-        for(int i=n-1;i>=0;--i)
-        {
-            if(nums[i]!=arr[i])
-            {
-                ei=i;
-                break;
-            }
-        }
-        if(si==-1&&ei==-1)
+        if(n==1)
         {
             return 0;
         }
-        return (ei-si)+1;
+        else
+        {
+            if(nums[0]>nums[1])
+            {
+                lnum=nums[0];
+            }
+            if(nums[n-1]<nums[n-2])
+            {
+                snum=nums[n-1];
+            }
+            for(int i=1;i<n-1;++i)
+            {
+                if(nums[i]>nums[i+1]||nums[i]<nums[i-1])
+                {
+                    snum=min(snum,nums[i]);
+                    lnum=max(lnum,nums[i]);
+                }
+            }
+            if(snum==INT_MAX&&lnum==INT_MIN)
+            {
+                return 0;
+            }
+            int lpos=-1;
+            int rpos=-1;
+            for(int i=0;i<n;++i)
+            {
+                if(snum<nums[i])
+                {
+                    lpos=i;
+                    break;
+                }
+            }
+            for(int i=n-1;i>0;--i)
+            {
+                if(lnum>nums[i])
+                {
+                    rpos=i;
+                    break;
+                }
+            }
+            return rpos-lpos+1;
+        }
+        
+        
     }
 };
