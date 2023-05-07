f47919ba4212c07dcc7fcb84a3ee1058cdf34d69
Akashdeep Tickoo <akashdeeptickoo2514@gmail.com>
2023-05-07 06:01:27 +0530

    Time: 67 ms (5.82%), Space: 41.2 MB (97.15%) - LeetHub

---
+++ 2667-create-hello-world-function/2667-create-hello-world-function.js
@@ -0,0 +1,14 @@
+/**
+ * @return {Function}
+ */
+var createHelloWorld = ()=>{
+    let ans=()=>{
+        return "Hello World";
+    }
+    return ans;
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
