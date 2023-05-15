57f9bf83365fd2d774cce58fc5d8a2ba7a062d7b
Akashdeep Tickoo <akashdeeptickoo2514@gmail.com>
2023-05-15 23:07:22 +0530

    Time: 60 ms (47.12%), Space: 41.8 MB (70.52%) - LeetHub

---
+++ 2666-allow-one-function-call/2666-allow-one-function-call.js
@@ -0,0 +1,26 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function(fn) {
+    let check=true;
+    return function(...args){
+        if(check)
+            {
+                check=false;
+                return fn(...args);
+            }
+        else
+            {
+                return undefined;
+            }
+    }
+};
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
